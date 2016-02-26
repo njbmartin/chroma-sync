b1136a07d6c0b9592e146ab872f09b1ed835ada2
Nicholas Martin <n.martin@movura.co.uk>
2016-02-26 21:12:31 +0000

    This could be important

--- MainApplication/PluginManager.cs
+++ MainApplication/PluginManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -8,9 +9,20 @@ using System.Threading.Tasks;
 
 namespace Ultrabox.ChromaSync
 {
+
+    
+
     class PluginManager
     {
-        private static List<Assembly> allAssemblies = new List<Assembly>();
+
+
+        public static List<Plugin> plugins = new List<Plugin>();
+
+        public class Plugin
+        {
+            public string Name;
+            public Assembly Assembly;
+        }
 
         public static void EnablePlugins()
         {
@@ -20,24 +32,27 @@ namespace Ultrabox.ChromaSync
             path = Path.Combine(path, "plugins");
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
-
             foreach (string dll in Directory.GetFiles(path, "*.dll"))
             {
                 var assembly = Assembly.LoadFile(dll);
-                allAssemblies.Add(assembly);
-
+                var plugin = new Plugin();
+                plugin.Name = dll;
+                plugin.Assembly = assembly;
+                plugins.Add(plugin);
+                Debug.WriteLine("found " + dll);
                 Type[] types = assembly.GetTypes();
                 foreach (Type type in types)
                 {
 
-                    if (type == null) return;
+                    if (type == null) continue;
 
                     MethodInfo methodInfo = type.GetMethod("AutoStart");
-                    if (methodInfo == null) return;
+                    if (methodInfo == null) continue;
+                    Debug.WriteLine(dll + " has AutoStart");
 
                     object result = null;
                     ParameterInfo[] parameters = methodInfo.GetParameters();
-                    object classInstance = Activator.CreateInstance(type, null);
+                    object classInstance = null; //Activator.CreateInstance(type, null);
                     if (parameters.Length == 0)
                         result = methodInfo.Invoke(classInstance, null);
                 }
